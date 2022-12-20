4166b9ddb4cba9290d364fb346f49baced176044
otdot <ottojuho@hotmail.fi>
2022-12-21 01:06:14 +0200

    removed unnecessary route

--- routes/drones.js
+++ routes/drones.js
@@ -15,14 +15,8 @@ router.get("/positions", async (_req, res) => {
 
 router.get("/cache", async (_req, res) => {
   const droneCache = await updateDroneCache();
-  console.log("keys: ", droneCache.keys());
   return res
     .json(droneCache.keys().map((key) => droneCache.get(key)))
     .status(200);
 });
-
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-});
-
 module.exports = router;
