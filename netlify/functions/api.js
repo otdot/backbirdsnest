56a25c0c6cfae259eb66d3dc61c685a3fc094395
otdot <ottojuho@hotmail.fi>
2022-12-29 01:05:13 +0200

    dist files

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,13 +2,13 @@ const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
 
-const droneRouter = require("../routes/drones");
+const droneRouter = require("../../routes/drones");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/.netlify/functions/drones", droneRouter);
+app.use("/.netlify/functions/api/drones", droneRouter);
 
 module.exports.handler = serverless(app);
