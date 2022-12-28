b6b74d44f69af008b8b78b641a9852f33c61e693
otdot <ottojuho@hotmail.fi>
2022-12-28 22:57:03 +0200

    updated clientside

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,5 @@
 const serverless = require("serverless-http");
-var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 var indexRouter = require("../routes/index");
@@ -11,32 +8,11 @@ var droneRouter = require("../routes/drones");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(cors());
-app.use(express.static("../dist"));
+app.use(express.static("dist"));
 
 app.use("/.netlify/functions", indexRouter);
 app.use("/.netlify/functions/drones", droneRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 module.exports.handler = serverless(app);
