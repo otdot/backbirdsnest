e302dbe9114e060a7bee82975ae2cf8005fafc5a
otdot <ottojuho@hotmail.fi>
2022-12-29 00:43:16 +0200

    dist files

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
-const indexRouter = require("./routes/index");
 const droneRouter = require("./routes/drones");
 
 const app = express();
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.static("dist"));
 
-app.use("/", indexRouter);
 app.use("/drones", droneRouter);
 
 // catch 404 and forward to error handler
--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,6 @@ const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
 
-const indexRouter = require("../routes/index");
 const droneRouter = require("../routes/drones");
 
 const app = express();
@@ -10,10 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/.netlify/functions", indexRouter);
-app.use("/.netlify/functions/test", (_req, res) => {
-  res.send("test response");
-});
 app.use("/.netlify/functions/drones", droneRouter);
 
 module.exports.handler = serverless(app);
