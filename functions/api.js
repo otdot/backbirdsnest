8668483438aae041273a97bf2bd1b698daab97fd
otdot <ottojuho@hotmail.fi>
2022-12-29 00:07:41 +0200

    updated clientside

--- functions/api.js
+++ functions/api.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/.netlify/functions", indexRouter);
+app.use("/.netlify/functions/test", (_req, res) => {
+  res.send("test response");
+});
 app.use("/.netlify/functions/drones", droneRouter);
 
 module.exports.handler = serverless(app);
