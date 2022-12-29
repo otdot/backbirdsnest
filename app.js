1827d8cf92ecb13a44e2ce1b861e0d59113890ed
otdot <ottojuho@hotmail.fi>
2022-12-29 11:11:43 +0200

    responsive styling

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
@@ -10,13 +8,8 @@ const droneRouter = require("./routes/drones");
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(cors());
 app.use(express.static("dist"));
 
@@ -28,7 +21,7 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
